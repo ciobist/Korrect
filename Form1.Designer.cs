c9c9d271b8cf7e1e7fae959acbce4410c30fe1b9
Nikita MIkhailov <nikita57979@gmail.com>
2024-06-12 14:07:03 +0300

    ,

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -549,7 +549,7 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(9, 116);
+            this.dataGridView1.Location = new System.Drawing.Point(40, 116);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
